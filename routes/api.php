fc3ce62ca7c4b4bac1d4e914cc3a17e7926cdc82
Sergey9891 <136695465+Sergey9891@users.noreply.github.com>
2023-06-28 22:08:30 +0300

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,15 @@
+<?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarBrandController;
+use App\Http\Controllers\CarModelController;
+use App\Http\Controllers\CarController;
+
+Route::get('/car-brands', [CarBrandController::class, 'index']);
+Route::get('/car-models', [CarModelController::class, 'index']);
+Route::get('/cars', [CarController::class, 'index']);
+Route::get('/cars/{id}', [CarController::class, 'show']);
+Route::post('/cars', [CarController::class, 'store']);
+Route::put('/cars/{id}', [CarController::class, 'update']);
+Route::delete('/cars/{id}', [CarController::class, 'destroy']);
